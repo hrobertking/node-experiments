cd146690e7433aeb775457439bb9b358a7e5a764
Robert King <hrobertking@cathmhaol.com>
2014-08-27 21:57:00 -0700

    Removing the query string reporting

--- router.js
+++ router.js
@@ -1,90 +1,113 @@
 /**
  * @author: hrobertking@cathmhoal.com
  *
+ * exports.on = subscribe;
  * exports.routes = routes;
  * exports.route = route;
  *
  */
 
-var fs = require('fs')
-  , path = require('path')
-  , querystring = require('querystring')
-  , url = require('url')
+var events = require('events')
   , writer = require('./writer')
-  , routes = {}
-  , uri                           // the uri requested - http://nodejs.org/api/url.html
-  , pathname                      // path
+  , emitter = new events.EventEmitter()     // event emitter
+  , routes = { }                            // routing table
+  , uri                                     // the uri requested - http://nodejs.org/api/url.html
 ;
 
-/**
- * Routes
- */
-routes['/favicon.ico'] = ignoreRequest;
+/* v -------------------------- ROUTING HANDLERS -------------------------------- v */
+routes['/favicon.ico'] = ignored;
 
+/**
+ * Ignores the request
+ * @return   {message}
+ * @param    {server.Message} message
+ * @emits    response-sent
+ */
+function ignored(message) {
+	emitter.emit('response-sent', writer.writeEmptyDocument(message));
+}
+
+/* ^ -------------------------- ROUTING HANDLERS -------------------------------- ^ */
+
+/**
+ * Handles requests not otherwise routed
+ * @return   {message}
+ * @param    {server.Message} message
+ * @emits    response-sent
+ */
+function unhandled(message) {
+	var fs = require('fs')
+	  , path = require('path')
+	  , filename = path.join(process.cwd(), uri.pathname)                 // the filename represented by the uri
+	;
+
+	try {
+		// check the path against the file system
+		fs.exists(filename, function(exists) {
+			if (exists && fs.statSync(filename).isDirectory()) {
+				filename += '/index.htm';
+				fs.exists(filename, function(exists) {
+					if (exists) {
+						emitter.emit('response-sent', writer.writeAsFile(message, fs.readFileSync(filename, 'binary')));
+					} else {
+						emitter.emit('response-sent', writer.writeNotFound(message));
+					}
+				});
+			} else if (exists) {
+				emitter.emit('response-sent', writer.writeAsFile(message, fs.readFileSync(filename, 'binary')));
+			} else {
+				emitter.emit('response-sent', writer.writeNotFound(message));
+			}
+		});
+	} catch (err) {
+		emitter.emit('error', writer.writeServerError(message, err));
+	}
+}
+
+/**
+ * The routing table
+ * @type     {object}
+ */
 Object.defineProperty(exports, 'routes', {
 	get: function() {
 		return routes;
 	}
 });
 
-function ignoreRequest(request, response) {
-	writer.writeContentType(response, 'html', 0);
-	writer.writeClose(response);
-}
-
-function defaultHandler(request, response) {
-	var filename = path.join(process.cwd(), pathname)                 // the filename represented by the uri
-	  , qs = querystring.parse((uri.search || '').replace(/\?/, ''))  // object representing the query string
-	  , cgivar                                                        // generic looping index
+/**
+ * Routes a request to a response
+ * @return   {void}
+ * @param    {server.Message} message
+ */
+function route(message) {
+	var url = require('url')
+	  , handler
 	;
 
-	// loop through all cgi variables
-	for (cgivar in qs) {
-		console.log(cgivar + ' = ' + qs[cgivar]);
-	}
+	// set the URI used by all the functions
+	uri = url.parse(message.request.url);
 
-	// check the path against the file system
-	fs.exists(filename, function(exists) {
-		if (exists && fs.statSync(filename).isDirectory()) {
-			filename += '/index.htm';
-			fs.exists(filename, function(exists) {
-				if (exists) {
-					fs.readFile(filename, 'binary', function(err, file) {
-						if (err) {
-							writer.writeServerError(response, err);
-						} else {
-							writer.writeAsFile(response, file);
-						}
-					});
-				} else {
-					writer.writeNotFound(response);
-				}
-			});
-		} else if (exists) {
-			fs.readFile(filename, 'binary', function(err, file) {
-				if (err) {
-					writer.writeServerError(response, err);
-				} else {
-					writer.writeAsFile(response, file);
-				}
-			});
-		} else {
-			writer.writeNotFound(response);
-		}
-	});
-}
-
-function route(request, response) {
-	var handler;
-
-	uri = url.parse(request.url);
-	pathname = uri.pathname;
-	handler = routes[pathname];
+	// get the handler for the requested resource
+	handler = routes[uri.pathname] || unhandled;
 
 	if (typeof handler === 'function') {
-		handler(request, response);
+		handler(message);
 	} else {
-		defaultHandler(request, response);
+		// oops.
+		emitter.emit('error', writer.writeServerError(message, 'Unable to route request'));
 	}
 }
 exports.route = route;
+
+/**
+ * Registers event handlers for request-received and response-sent events
+ * @return   {void}
+ * @param    {string} eventname
+ * @param    {function} handler
+ */
+function subscribe(eventname, handler) {
+	if ((/request\-received|response\-sent/).test(eventname)) {
+		emitter.on(eventname, handler);
+	}
+}
+exports.on = subscribe;
