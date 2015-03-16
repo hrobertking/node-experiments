c11f8569d02b4efe105ac71e0a1871aa17002bc8
Robert King <hrobertking@cathmhaol.com>
2015-03-16 15:26:43 -0700

    Update cli-opts.js

--- cli-opts.js
+++ cli-opts.js
@@ -11,54 +11,84 @@
  * @example  node index.js --port 9090 -u hrobertking --password foobar -> cli.parse(process.argv.slice(2)) -> { 'password':'foobar', 'port':'9090', 'u':'hrobertking' }
  */
 function parse(args) {
-  var obj = { argv:args }
+  var isfirst = true
     , arg_l
     , arg_s
-    , values
+    , value
+    , obj = { argv:[ ] }
   ;
-
-  // If the first two args are the script call, drop them
-  if (args[0] === 'node' && args[1].indexOf('.js')) {
-    obj._command = args[1];
-    args.splice(0, 2);
-    obj.argv = args;
-  }
 
   // Loop through all the options args
   while (args.length) {
+    // delete the word 'node' if it's the first arg
+    if (args[0] === 'node' && isfirst) {
+      args.splice(0, 1);
+    }
+    // if the first arg is a js filename, it's the caller
+    if (args[0].indexOf('.js') > -1 && isfirst) {
+      obj._command = args[0];
+      args.splice(0, 1);
+    }
     arg_l = /^\-{2}(\S+)/.exec(args[0]);  // long arg format
     arg_s = /^\-{1}(\S+)/.exec(args[0]);  // short arg format
     if (arg_l) {
-      if (!/^\-{2}(\S+)/.test(args[1])) {
-        // there is data passed in, so get the current value for the arg and 
+      if (!(/^\-{2}(\S+)/).test(args[1]) && !(/^\-{1}(\S+)/).test(args[1])) {
+        // there is data passed in, so get the current value for the arg and
         // add this value to it, then store the values in the property of the
         // object, but assume it's a flag if the value is empty
-        values = (obj[arg_l[1]] || '').split(',');
-        values.push(args[1]);
-        obj[arg_l[1]] = values.join(',').replace(/^\,/, '').replace(/\,$/, '');
+        value = (obj[arg_l[1]] || '').split(',');
+        value.push(args[1]);
+        obj[arg_l[1]] = value.join(',').replace(/^\,/, '').replace(/\,$/, '');
         obj[arg_l[1]] = obj[arg_l[1]].length ? obj[arg_l[1]] : true;
-        args.splice(0, 2);
+        obj.argv.push(args[0]);
+        args.splice(0, 1);
+        obj.argv.push(args[0]);
+        args.splice(0, 1);
       } else {
         // there isn't data passed in, so set the flag to true
         obj[arg_l[1]] = true;
+        obj.argv.push(args[0]);
         args.splice(0, 1);
       }
     } else if (arg_s) {
-      // split the 'small' arg name into letters, then loop through the letters
-      // and set each to true
+      // split the 'small' arg name into letters
       arg_s = arg_s[1].split('');
-      for (values = 0; values < arg_s.length; values += 1) {
-        obj[arg_s[values]] = true;
+      // if the arg is more than one letter, then loop through the letters
+      // and set each to true
+      if (arg_s.length > 1) {
+        for (value = 0; value < arg_s.length; value += 1) {
+          obj[arg_s[value]] = true;
+        }
+        obj.argv.push(args[0]);
+        args.splice(0, 1);
+      } else if (!(/^\-{2}(\S+)/).test(args[1]) && !(/^\-{1}(\S+)/).test(args[1])) {
+        // there is data passed in, so get the current value for the arg and
+        // add this value to it, then store the values in the property of the
+        // object, but assume it's a flag if the value is empty
+        value = (obj[arg_s[0]] || '').split(',');
+        value.push(args[1]);
+        obj[arg_s[0]] = value.join(',').replace(/^\,/, '').replace(/\,$/, '');
+        obj[arg_s[0]] = obj[arg_s[0]].length ? obj[arg_s[0]] : true;
+        obj.argv.push(args[0]);
+        args.splice(0, 1);
+        obj.argv.push(args[0]);
+        args.splice(0, 1);
+      } else {
+        // there isn't data passed in, so set the flag to true
+        obj[arg_s[0]] = true;
+        obj.argv.push(args[0]);
+        args.splice(0, 1);
       }
-      args.splice(0, 1);
     } else {
       // get the current value for the unnamed arguments and add this value to
       // it, then store the values in the property of the object
-      values = (obj._unnamed || '').split(',');
-      values.push(args[0]);
-      obj._unnamed = values.join(',').replace(/^\,/, '').replace(/\,$/, '');
+      value = (obj._unnamed || '').split(',');
+      value.push(args[0]);
+      obj._unnamed = value.join(',').replace(/^\,/, '').replace(/\,$/, '');
+      obj.argv.push(args[0]);
       args.splice(0, 1);
     }
+    isfirst = false;
   }
 
   return obj;
