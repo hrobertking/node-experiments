5e1b35d08844bea43dae6ec4f83e4508ceb15f67
Robert King <hrobertking@cathmhaol.com>
2016-08-21 17:09:52 -0700

    Adding ability to turn off logging

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ server.log = path.join(__dirname, 'logs/' + (cli_args.log || 'server.log'));
 server.port = cli_args.p || cli_args.port || 8000;
 server.router.dir = cli_args.d || cli_args.dir || cli_args.directory;
 
+if (cli_args.no_log) {
+  server.logging = false;
+}
+
 // Usage information
 function usage() {
   console.log('Syntax: ' + invoked.split(path.sep).pop() + ' <PARAMETERS>');
@@ -27,6 +31,7 @@ function usage() {
   console.log('-[-]d[ir[ectory]] <PATH>   Sets the directory from where pages are served. Default is ' + process.cwd());
   console.log('-[-]h[elp]                 Show this usage message');
   console.log('-[-]log] <FILENAME>        Sets the filename for the server log. Default is server.log in the app directory');
+  console.log('-[-]no_log                 Turns off logging');
   console.log('-[-]p[ort] <PORT_NUMBER>   Sets the port the application listens on. Default is 8000');
   console.log('');
 }
@@ -35,6 +40,6 @@ if (cli_args.h || cli_args.help) {
   usage();
 } else {
   server.start();
-  console.log('Server started on ' + server.port + '\n');
+  console.log('Server started on ' + server.host + ':' + server.port + '\n');
 }
 
